047fefb838f3f3fd87dda43215b81870fa9a0e87
Michael Kemp <mkemp@accu-time.com>
2017-10-20 14:09:11 -0400

    Initial commit

---
+++ html/src/com/mygdx/uidemo/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.uidemo.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.uidemo.UIDemo;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new UIDemo();
+        }
+}
